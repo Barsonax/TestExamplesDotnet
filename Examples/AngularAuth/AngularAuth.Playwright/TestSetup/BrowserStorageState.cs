cdf9b0597c57ef3b4be62d089fd5b5ee2b90ec98
Rick <rick-van-dam@outlook.com>
2023-12-04 21:18:26 +0100

    move code to separate files

---
+++ Examples/AngularAuth/AngularAuth.Playwright/TestSetup/BrowserStorageState.cs
@@ -0,0 +1,17 @@
+﻿namespace AngularAuth.Playwright.TestSetup;
+
+public class BrowserStorageState
+{
+    public BrowserStorageStateOrigin[] Origins { get; init; } = Array.Empty<BrowserStorageStateOrigin>();
+}
+
+public class BrowserStorageStateOrigin
+{
+    public required string Origin { get; init; }
+    public required BrowserStorageStateLocalStorage[] LocalStorage { get; init; }
+}
+public class BrowserStorageStateLocalStorage
+{
+    public required string Name { get; init; }
+    public required string Value { get; init; }
+}
--- Examples/AngularAuth/AngularAuth.Playwright/TestSetup/TestBase.cs
+++ Examples/AngularAuth/AngularAuth.Playwright/TestSetup/TestBase.cs
@@ -1,5 +1,4 @@
-﻿using System.Globalization;
-using System.Security.Claims;
+﻿using System.Security.Claims;
 using System.Text.Json;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Playwright;
@@ -35,56 +34,13 @@ public abstract class TestBase : BrowserTest
     };
     private BrowserNewContextOptions ContextOptions()
     {
-        var environment = "login.windows.net";
-        var clientId = "foobar";
-        var localAccountId = Guid.NewGuid().ToString();
-        var tenantId = Guid.NewGuid().ToString();
-        var homeAccountId = $"{localAccountId}.{tenantId}";
-        var jwtToken = TestJwtGenerator.GenerateJwtToken(Array.Empty<Claim>());
-
-        var accessToken = new MsalAccessToken
-        {
-            ClientId = clientId,
-            HomeAccountId = homeAccountId,
-            Realm = tenantId,
-            Secret = jwtToken,
-            Target = "User.Read"
-        };
-
-        var accountInfo = new MsalAccountInfo
-        {
-            HomeAccountId = homeAccountId,
-            LocalAccountId = localAccountId,
-            Realm = tenantId,
-            TenantProfiles = new MsalTenantProfile[]
-            {
-                new()
-                {
-                    LocalAccountId = localAccountId,
-                    TenantId = tenantId
-                }
-            },
-        };
-
         var storageState = new BrowserStorageState {
             Origins = new BrowserStorageStateOrigin[]
             {
                 new()
                 {
                     Origin = Sut.ServerAddress,
-                    LocalStorage = new BrowserStorageStateLocalStorage[]
-                    {
-                        new()
-                        {
-                            Name = $"{localAccountId}.{tenantId}-{environment}-accesstoken-{clientId}-{tenantId}",
-                            Value = JsonSerializer.Serialize(accessToken, JsonSerializerOptions)
-                        },
-                        new()
-                        {
-                            Name = $"{localAccountId}.{tenantId}-{environment}-{tenantId}",
-                            Value = JsonSerializer.Serialize(accountInfo, JsonSerializerOptions)
-                        },
-                    }
+                    LocalStorage = MsalLocalStorageTestGenerator.Generate(Array.Empty<Claim>())
                 }
             }
         };
@@ -118,55 +74,4 @@ public abstract class TestBase : BrowserTest
             await Task.Delay(200);
         }
     }
-}
-public class MsalAccountInfo
-{
-    public string AuthorityType { get; init; } = "foobar";
-    public string ClientInfo { get; init; } = "foobar";
-    public string Environment { get; init; } = "login.windows.net";
-    public required string HomeAccountId { get; init; }
-    public required string LocalAccountId { get; init; }
-    public string Name { get; init; } = "foobar";
-    public required string Realm { get; init; }
-    public required MsalTenantProfile[] TenantProfiles { get; init; }
-    public string Username { get; init; } = "foo@bar.com";
-}
-
-public class MsalTenantProfile
-{
-    public bool IsHomeTenant { get; init; } = true;
-    public required string LocalAccountId { get; init; }
-    public string Name { get; init; } = "foobar";
-    public required string TenantId { get; init; }
-}
-
-public class MsalAccessToken
-{
-    public string CachedAt { get; init; } = DateTimeOffset.MinValue.ToUnixTimeSeconds().ToString(CultureInfo.InvariantCulture);
-    public required string ClientId { get; init; }
-    public string CredentialType { get; init; } = "AccessToken";
-    public string Environment { get; init; } = "login.windows.net";
-    public string ExpiresOn { get; init; } = DateTimeOffset.MaxValue.ToUnixTimeSeconds().ToString(CultureInfo.InvariantCulture);
-    public string ExtendedExpiresOn { get; init; } = DateTimeOffset.MaxValue.ToUnixTimeSeconds().ToString(CultureInfo.InvariantCulture);
-    public required  string HomeAccountId { get; init; }
-    public required string Realm { get; init; }
-    public required string Secret { get; init; }
-    public required string Target { get; init; }
-    public string TokenType { get; init; } = "Bearer";
-}
-
-public class BrowserStorageState
-{
-    public BrowserStorageStateOrigin[] Origins { get; init; } = Array.Empty<BrowserStorageStateOrigin>();
-}
-
-public class BrowserStorageStateOrigin
-{
-    public required string Origin { get; init; }
-    public required BrowserStorageStateLocalStorage[] LocalStorage { get; init; }
-}
-public class BrowserStorageStateLocalStorage
-{
-    public required string Name { get; init; }
-    public required string Value { get; init; }
 }
