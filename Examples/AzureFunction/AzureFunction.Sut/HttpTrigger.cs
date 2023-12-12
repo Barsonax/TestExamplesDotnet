79285507676e64ff4c5bbe78f7c5c793f4a3902e
Rick van Dam <rick-van-dam@outlook.com>
2023-12-12 16:39:38 +0100

    setup azure function sut

---
+++ Examples/AzureFunction/AzureFunction.Sut/HttpTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace AzureFunction.Sut;
+
+public class HttpTrigger
+{
+    private readonly ILogger _logger;
+
+    public HttpTrigger(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<HttpTrigger>();
+    }
+
+    [Function("HttpTrigger")]
+    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+        FunctionContext executionContext)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+        response.WriteString("Welcome to Azure Functions!");
+
+        return response;
+
+    }
+}
